61df55a3f3f99adb47439b622882fd7d746976a2
youswim96@gmail.com <youswim96@gmail.com>
2022-05-13 23:51:41 +0900

    10

--- src/main/java/hello/hellospring/HelloController.java
+++ src/main/java/hello/hellospring/HelloController.java
@@ -1,2 +1,15 @@
-package hello.hellospring;public class HelloController {
+package hello.hellospring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!");
+        return "hello";
+    }
 }
